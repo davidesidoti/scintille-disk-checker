1b31006754ced9c6e317cbaa9661f18d5a153c51
hash_developer <63818411+davidesidoti@users.noreply.github.com>
2022-07-21 17:07:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,19 +9,3 @@ total = total // (2**30)
 
 free = free // (2**30)
 free = round(free / total * 100, 2)
-
-if (free > 65):
-    msg = EmailMessage()
-    msg.set_content(
-        f'Lo spazio del disco in `{socket.gethostname()}` è al {free}%')
-    msg['Subject'] = f'Spazio del disco in {socket.gethostname()} basso'
-    msg['From'] = 'disk-checker@scintille.net'
-    msg['To'] = 'davide@scintille.net'
-
-    s = smtplib.SMTP('localhost')
-    s.send_message(msg)
-    s.quit()
-
-    print("Disk is low")
-else:
-    print("Disk is ok")
