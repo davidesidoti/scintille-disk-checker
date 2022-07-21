dea252c1c12f5f4cb5ed733756d429016617cba9
hash_developer <63818411+davidesidoti@users.noreply.github.com>
2022-07-21 10:02:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import shutil
+import socket
+import smtplib
+from email.message import EmailMessage
 
 total, used, free = shutil.disk_usage("/")
 
-print("Total: %d GiB" % (total // (2**30)))
-print("Used: %d GiB" % (used // (2**30)))
-print("Free: %d GiB" % (free // (2**30)))
+total = total // (2**30)
+
+free = free // (2**30)
+free = round(free / total * 100, 2)
+
+if (free > 65):
+    msg = EmailMessage()
+    msg.set_content(
+        f'Lo spazio del disco in `{socket.gethostname()}` è al {free}%')
+    msg['Subject'] = f'Spazio del disco in {socket.gethostname()} basso'
+    msg['From'] = 'disk-checker@scintille.net'
+    msg['To'] = 'davide@scintille.net'
+
+    s = smtplib.SMTP('localhost')
+    s.send_message(msg)
+    s.quit()
+
+    print("Disk is low")
+else:
+    print("Disk is ok")
